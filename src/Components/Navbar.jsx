858bd7778d1a3a8855302d5a45f2c9953e9fca03
mohammadmunnamia0 <mohammadmunnamia0@gmail.com>
2025-03-02 23:14:31 +0600

    `Update Navbar component links in horizontal menu`

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -50,21 +50,21 @@ function Navbar() {
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Home</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-            <li>
-              <a>Service</a>
-            </li>
-            <li>
-              <a>Project</a>
-            </li>
-            <li>
-              <a>Blog</a>
-            </li>
+          <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Service</a>
+              </li>
+              <li>
+                <a>Project</a>
+              </li>
+              <li>
+                <a>Blog</a>
+              </li>
           </ul>
         </div>
         <div className="navbar-end">
